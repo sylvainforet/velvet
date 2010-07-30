49cfe3b90699e6d39346a5d7f7ee00811b349c07
sylvainforet <sylvainforet@gmail.com>
2010-07-30 16:04:40 +1000

    Copyright GPL thingy

--- src/kmerHashTable.c
+++ src/kmerHashTable.c
@@ -1,10 +1,27 @@
-/**
- *
- */
+/*
+Copyright 2009 Sylvain Foret (sylvain.foret@anu.edu.au) 
+
+    This file is part of Velvet.
+
+    Velvet is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Velvet is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Velvet; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
 
 /**
  * Hash table with direct addressing and quadratic probing
- * Adapted from Glib's GHash table
+ * Loosely based from Glib's GHash table
  * http://www.gtk.org
  * This version has `in-line' nodes, no tombstones and resizes in place
  */
