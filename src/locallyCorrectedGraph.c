6c50e5501dcbf743f65f6383c23e7804fec8c240
sylvainforet <sylvainforet@gmail.com>
2011-04-06 16:21:55 +1000

    Back to the previous gap scheme

--- src/locallyCorrectedGraph.c
+++ src/locallyCorrectedGraph.c
@@ -18,6 +18,7 @@ Copyright 2007, 2008 Daniel Zerbino (zerbino@ebi.ac.uk)
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 */
+#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -30,6 +31,8 @@ Copyright 2007, 2008 Daniel Zerbino (zerbino@ebi.ac.uk)
 #include "shortReadPairs.h"
 #include "locallyCorrectedGraph.h"
 #include "utility.h"
+
+extern float roundf(float);
 
 static const Time INDEL = 0;
 static const Time SIM[4][4] = {
@@ -141,7 +144,7 @@ extractSequence(PassageMarkerI path, TightString * sequence)
 	return true;
 }
 
-static Time max(Time A, Time B, Time C)
+static Time max3(Time A, Time B, Time C)
 {
 	if (A >= B && A >= C)
 		return A;
@@ -184,14 +187,19 @@ compareSequences(TightString * sequence1, TightString * sequence2)
 			    [(int) getNucleotide(j - 1, sequence2)];
 			Choice2 = Fmatrix[i - 1][j] + INDEL;
 			Choice3 = Fmatrix[i][j - 1] + INDEL;
-			Fmatrix[i][j] = max(Choice1, Choice2, Choice3);
+			Fmatrix[i][j] = max3(Choice1, Choice2, Choice3);
 		}
 	}
 
 	maxScore = Fmatrix[length1][length2];
 
-	if (maxScore < maxLength - MAXGAPS)
-		return false;
+	if (maxLength < 100) {
+		if (maxScore < maxLength - MAXGAPS)
+			return false;
+	} else {
+		if (roundf((float)(MAXGAPS * maxLength) / 100.) < maxLength - maxScore)
+			return false;
+	}
 
 	if ((1 - maxScore / maxLength) > MAXDIVERGENCE)
 		return false;
